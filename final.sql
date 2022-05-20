4d23e7ec5a225c60703c7a1768e82571f61971ca
kjohnson422 <73547881+kjohnson422@users.noreply.github.com>
2022-05-19 22:10:40 -0400

    Add files via upload

---
+++ final.sql
@@ -0,0 +1,39 @@
+create table actors
+(
+    id         int auto_increment,
+    first_name varchar(20) null,
+    last_name  varchar(30) null,
+    MovieId    int         not null,
+    constraint id
+        unique (id)
+);
+
+create table movies
+(
+    id           int auto_increment
+        primary key,
+    Title        varchar(30) null,
+    release_year varchar(4)  null,
+    genre        varchar(20) null,
+    MovieId      int         not null,
+    ActorsId     int         not null,
+    constraint id
+        unique (id)
+);
+
+create table directors
+(
+    id         int auto_increment,
+    first_name varchar(20) null,
+    last_name  varchar(30) null,
+    MovieId    int         not null,
+    constraint id
+        unique (id),
+    constraint directors_ibfk_1
+        foreign key (MovieId) references movies (id)
+);
+
+create index MovieId
+    on directors (MovieId);
+
+
